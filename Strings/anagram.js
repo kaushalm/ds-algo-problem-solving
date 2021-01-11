389778d5d50f04d0ddf782454d2282c724c92b74
Kaushal Mehra <kaushalmehra1987@gmail.com>
2021-01-11 10:05:38 +0530

    anagram

--- Strings/anagram.js
+++ Strings/anagram.js
@@ -1,1 +1,44 @@
-console.log('test');
+//find if given string si n anagram or not
+//an anagram are words which can be formed from each other
+//any character in 256 set of characters
+
+function isAnagram (word1, word2) {
+
+    if(word1.length !== word2.length){
+        console.log('NOT anagram');
+        return false;
+    }
+
+    word1 = word1.toLowerCase();
+    word2 = word2.toLowerCase();
+
+    let lookup ={};
+
+    for(let i=0; i<word1.length; i++){
+        let letter = word1[i]
+        if(lookup[word1[i]]){
+
+            lookup[word1[i]] +=1; 
+        } else {
+            lookup[word1[i]] =1;
+        }
+    }
+
+    for(let j=0; j<word2.length; j++){
+        if(!lookup[word2[j]]){
+            console.log('NOT anagram');
+            return false
+        } else{
+            lookup[word2[j]] -=1;
+        }
+    }
+
+    console.log('anagram');
+    return true;
+
+    //console.log(lookup);
+    //console.log(word2);
+
+}
+
+isAnagram('banana', 'BANANA');
